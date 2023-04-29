18c74f6810cf296fb08e5db0d085009852ba3a9e
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 16:52:02 +0300

    updated controller

--- user-service/src/main/java/com/danis0n/controller/UserController.java
+++ user-service/src/main/java/com/danis0n/controller/UserController.java
@@ -1,18 +1,18 @@
 package com.danis0n.controller;
 
-import com.danis0n.dto.AuthenticationData;
 import com.danis0n.dto.CreateUserDto;
 import com.danis0n.dto.UserDto;
 import com.danis0n.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("api/v1/user")
 public class UserController {
 
     private final UserService service;
@@ -22,23 +22,15 @@ public class UserController {
         return ResponseEntity.ok("Hello fro user-controller");
     }
 
-    @PostMapping
-    public ResponseEntity<UserDto> create(@RequestBody CreateUserDto dto) {
-        return ResponseEntity.ok(service.create(dto));
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("register")
+    public void create(@RequestBody CreateUserDto dto) {
+        service.addUser(dto);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<UserDto> findUserByUUID(@PathVariable("id") String uuid) {
         return ResponseEntity.ok(service.findUserByUUID(uuid));
-    }
-
-    @GetMapping("retrieve-credentials")
-    public ResponseEntity<AuthenticationData> retrieveCredentialsByUsername(@RequestParam("username") String username) {
-        return ResponseEntity.ok(
-                AuthenticationData.builder()
-                    .username(username)
-                    .hashedPassword("1234")
-                    .build());
     }
 
 }
