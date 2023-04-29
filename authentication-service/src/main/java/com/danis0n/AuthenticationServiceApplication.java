b2609dc9903c93139f5f4257d250dcc61f687ff8
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 17:15:25 +0300

    remove unused annotation

--- authentication-service/src/main/java/com/danis0n/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/danis0n/AuthenticationServiceApplication.java
@@ -3,10 +3,8 @@ package com.danis0n;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-//@EnableRedisHttpSession
 @EnableDiscoveryClient
 public class AuthenticationServiceApplication {
 
