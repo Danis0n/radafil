08cb68a3fc63135d6b60e553623a3d9ac628777d
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 16:31:15 +0300

    updated open api routes

--- api-gateway/src/main/java/com/danis0n/filter/RouteValidator.java
+++ api-gateway/src/main/java/com/danis0n/filter/RouteValidator.java
@@ -10,13 +10,14 @@ import java.util.function.Predicate;
 public class RouteValidator {
 
 //    TODO: move to yaml file with openApiEndpoints
-    public static final List<String> openApiEndpoints = List.of(
-//            "/api/v1/auth/system",
-//            "/api/v1/auth/user"
+    public static final List<String> OPEN_API_ENDPOINTS = List.of(
+            "/api/v1/user/register",
+            "/api/v1/auth/login",
+            "/api/v1/user/find-by-name"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
-            request -> openApiEndpoints
+            request -> OPEN_API_ENDPOINTS
                     .stream()
                     .noneMatch(uri -> request.getURI().getPath().contains(uri));
 
