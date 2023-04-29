b533c48b0fb8633894b8d88cb0eedc3841c7222e
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 19:57:28 +0300

    updated return value

--- authentication-service/src/main/java/com/danis0n/service/session/SessionService.java
+++ authentication-service/src/main/java/com/danis0n/service/session/SessionService.java
@@ -12,14 +12,13 @@ public class SessionService {
 
     private final SessionDao sessionDao;
 
-    public void save(String uuid, String username) {
+    public Boolean save(String uuid, String username) {
         Session session = new Session(uuid, username);
-        sessionDao.save(session);
-
+        return sessionDao.save(session);
     }
 
-    public void delete(String uuid) {
-        sessionDao.delete(uuid);
+    public Boolean delete(String uuid) {
+        return sessionDao.delete(uuid);
     }
 
 }
