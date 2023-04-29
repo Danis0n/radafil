2ce2144b962a930f98c26393ba38900795364f8b
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 17:04:36 +0300

    fixed database sql script to create simple tables

--- user-service/sql/create.sql
+++ user-service/sql/create.sql
@@ -1,16 +1,12 @@
-CREATE TABLE app_user (
-    uuid UUID NOT NULL PRIMARY KEY,
-    username VARCHAR(32) NOT NULL,
+CREATE TABLE app_admin (
+    uuid UUID NOT NULL UNIQUE PRIMARY KEY,
+    username VARCHAR(32) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE role (
-    id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(10) NOT NULL
-);
-
-CREATE TABLE user_role (
-    user_id UUID NOT NULL REFERENCES app_user(uuid),
-    role_id INTEGER NOT NULL REFERENCES role(id),
-    CONSTRAINT user_role_pkey PRIMARY KEY(user_id, role_id)
+CREATE TABLE app_user (
+    uuid UUID NOT NULL UNIQUE PRIMARY KEY,
+    username VARCHAR(32) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(50) UNIQUE
 );
