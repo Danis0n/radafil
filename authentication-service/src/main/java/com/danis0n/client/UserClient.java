0bad48e5d89a38b6efe9bd80c1685b3bf6cd0537
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 16:47:31 +0300

    added method for retrieving user credentials

--- authentication-service/src/main/java/com/danis0n/client/UserClient.java
+++ authentication-service/src/main/java/com/danis0n/client/UserClient.java
@@ -1,6 +1,6 @@
 package com.danis0n.client;
 
-import com.danis0n.dto.AuthenticationData;
+import com.danis0n.entity.AuthenticationData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
@@ -9,7 +9,10 @@ import org.springframework.web.service.annotation.HttpExchange;
 @HttpExchange
 public interface UserClient {
 
-    @GetExchange("/api/v1/user/retrieve-credentials")
-    ResponseEntity<AuthenticationData> retrieveCredentialsByUsername(@RequestParam("username") String username);
+    @GetExchange("/api/v1/credentials/admin")
+    ResponseEntity<AuthenticationData> retrieveAdminCredentialsByUsername(@RequestParam("username") String username);
+
+    @GetExchange("/api/v1/credentials/user")
+    ResponseEntity<AuthenticationData> retrieveUserCredentialsByUsername(@RequestParam("username") String username);
 
 }
