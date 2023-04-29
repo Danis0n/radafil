edf7db3e5aa3d46811b48e702bf366dcf197356d
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 17:07:59 +0300

    added login and logout methods

--- authentication-service/src/main/java/com/danis0n/controller/AuthController.java
+++ authentication-service/src/main/java/com/danis0n/controller/AuthController.java
@@ -1,40 +1,55 @@
 package com.danis0n.controller;
 
+import com.danis0n.dto.LoginRequestDto;
+import com.danis0n.service.authentication.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("api/v1/auth")
 public class AuthController {
+
+    private final AuthenticationService service;
 
     @GetMapping
     public String test() {
         return "HELLO FROM AUTH MICRO-SERVICE";
     }
 
-    @PostMapping("/register")
-    public void register() {}
-
-
-    @GetMapping("/system")
-    @PreAuthorize("hasRole('SYSTEM')")
-    public ResponseEntity<Boolean> system(Authentication authentication) {
-        return ResponseEntity.ok(Boolean.TRUE);
+    @PostMapping("login")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto request, HttpServletResponse response) {
+        return ResponseEntity.ok(service.login(request, response));
     }
 
-    @GetMapping("/user")
+    @PostMapping("logout")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<Boolean> validateUser(Authentication authentication) {
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Boolean> logout(HttpServletRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok(service.logout(request, response));
+    }
+
+    @GetMapping("system")
+    @PreAuthorize("hasRole('SYSTEM')")
+    public ResponseEntity<Boolean> validateSystem() {
         return ResponseEntity.ok(Boolean.TRUE);
     }
 
-    @GetMapping("/admin")
+    @GetMapping("user")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Boolean> validateUser() {
+        return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @GetMapping("admin")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Boolean> validateAdmin(Authentication authentication) {
+    public ResponseEntity<Boolean> validateAdmin() {
         return ResponseEntity.ok(Boolean.TRUE);
     }
 }
