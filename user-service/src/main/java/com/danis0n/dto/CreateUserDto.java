fbdd3adc5e06265dad79050586e6f8eac6d0dd75
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 17:12:25 +0300

    added validation fields

--- user-service/src/main/java/com/danis0n/dto/CreateUserDto.java
+++ user-service/src/main/java/com/danis0n/dto/CreateUserDto.java
@@ -1,9 +1,24 @@
 package com.danis0n.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
+@AllArgsConstructor
 public class CreateUserDto {
+
+    @NotNull
+    @NotEmpty
     private String username;
+
+    @NotNull
+    @NotEmpty
     private String password;
+
+    @Email(message = "invalid email")
+    private String email;
 }
