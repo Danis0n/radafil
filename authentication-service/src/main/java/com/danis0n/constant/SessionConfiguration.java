b886a931d8272f4c0a46f88f4599e8670f0a4268
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-04-29 23:13:32 +0300

    replaced with class

---
+++ authentication-service/src/main/java/com/danis0n/constant/SessionConfiguration.java
@@ -0,0 +1,8 @@
+package com.danis0n.constant;
+
+public class SessionConfiguration {
+
+    public static final String SESSION_NAME = "session";
+    public static final Integer SESSION_EXPIRE_TIME_IN_DAYS = 10;
+    public static final Integer SESSION_EXPIRE_TIME_IN_SECONDS = 60 * 60 * 24 * 10;
+}
