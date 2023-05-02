c7029aa34de478084513bbe4bfa1d5124da6d56f
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-02 13:46:46 +0300

    removed mapper class and used static method

--- user-service/src/main/java/com/danis0n/service/UserServiceImpl.java
+++ user-service/src/main/java/com/danis0n/service/UserServiceImpl.java
@@ -4,13 +4,14 @@ import com.danis0n.dto.CreateUserDto;
 import com.danis0n.dto.UserDto;
 import com.danis0n.entity.User;
 import com.danis0n.exception.DuplicateResourceException;
-import com.danis0n.mapper.UserMapper;
 import com.danis0n.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static com.danis0n.util.Mapper.mapUserToUserDto;
 
 
 @Service
@@ -19,12 +20,11 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
     private final PasswordEncoder BCRYPT;
-    private final UserMapper mapper;
 
     @Override
     public UserDto findUserByUUID(String uuid) {
         return repository.findById(uuid)
-                .map(mapper)
+                .map(mapUserToUserDto)
                 .orElseThrow(() -> new EntityNotFoundException(
                         "User with id [%s] not found".formatted(uuid)
                 ));
