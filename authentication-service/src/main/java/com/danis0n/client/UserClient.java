f4e3b4c6beba294aaeac6028bcbf40b00a2284bd
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-02 13:44:50 +0300

    fixed names

--- authentication-service/src/main/java/com/danis0n/client/UserClient.java
+++ authentication-service/src/main/java/com/danis0n/client/UserClient.java
@@ -1,6 +1,6 @@
 package com.danis0n.client;
 
-import com.danis0n.entity.AuthenticationData;
+import com.danis0n.entity.AuthenticationDataDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
@@ -10,9 +10,9 @@ import org.springframework.web.service.annotation.HttpExchange;
 public interface UserClient {
 
     @GetExchange("/api/v1/credentials/admin")
-    ResponseEntity<AuthenticationData> retrieveAdminCredentialsByUsername(@RequestParam("username") String username);
+    ResponseEntity<AuthenticationDataDto> retrieveAdminCredentialsByUsername(@RequestParam("username") String username);
 
     @GetExchange("/api/v1/credentials/user")
-    ResponseEntity<AuthenticationData> retrieveUserCredentialsByUsername(@RequestParam("username") String username);
+    ResponseEntity<AuthenticationDataDto> retrieveUserCredentialsByUsername(@RequestParam("username") String username);
 
 }
--- authentication-service/src/main/java/com/danis0n/util/Mapper.java
+++ authentication-service/src/main/java/com/danis0n/util/Mapper.java
@@ -1,6 +1,6 @@
 package com.danis0n.util;
 
-import com.danis0n.entity.AuthenticationData;
+import com.danis0n.entity.AuthenticationDataDto;
 import com.danis0n.entity.User;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +9,7 @@ import java.util.function.Function;
 @Component
 public class Mapper {
 
-    public static Function<AuthenticationData, User> mapDataToUser =
+    public static Function<AuthenticationDataDto, User> mapDataToUser =
             data -> new User(data.getUsername(), data.getHashedPassword());
 
 }
