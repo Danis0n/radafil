1687d942edb2127a373727863bf290a86961452e
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-02 21:45:35 +0300

    added new exceptions handler

--- user-service/src/main/java/com/danis0n/handler/RestResponseEntityExceptionHandler.java
+++ user-service/src/main/java/com/danis0n/handler/RestResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
-package com.danis0n.exception.handler;
+package com.danis0n.handler;
 
 import com.danis0n.entity.ErrorMessage;
 import com.danis0n.exception.DuplicateResourceException;
+import com.danis0n.exception.ObjectNotValidException;
+import com.danis0n.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +19,20 @@ public class RestResponseEntityExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DuplicateResourceException.class)
     public ErrorMessage userDuplicateResourceHandler(DuplicateResourceException exception) {
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorMessage userNotFoundHandler(UserNotFoundException exception) {
+        return new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ObjectNotValidException.class)
+    public ErrorMessage objectNotValidHandler(ObjectNotValidException exception) {
         return new ErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
