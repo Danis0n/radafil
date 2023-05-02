b9c09df95d1b543f30581a2ebb7510250053ad5b
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-02 21:44:39 +0300

    removed annotations

--- user-service/src/main/java/com/danis0n/repository/UserRepository.java
+++ user-service/src/main/java/com/danis0n/repository/UserRepository.java
@@ -6,14 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    @Override
-    @NonNull
-    Optional<User> findById(@NonNull String uuid);
-
+    Optional<User> findByUuid(@NonNull UUID uuid);
     Optional<User> findByEmail(@NonNull String email);
     Optional<User> findByUsername(@NonNull String username);
 
