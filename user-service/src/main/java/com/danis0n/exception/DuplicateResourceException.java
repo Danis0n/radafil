4e1dcf14f36327a0ff833e5bf4e041dfe06cc9eb
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-02 21:47:45 +0300

    fixed naming in constructor

--- user-service/src/main/java/com/danis0n/exception/DuplicateResourceException.java
+++ user-service/src/main/java/com/danis0n/exception/DuplicateResourceException.java
@@ -1,8 +1,7 @@
 package com.danis0n.exception;
 
 public class DuplicateResourceException extends RuntimeException {
-
-    public DuplicateResourceException(String message) {
-        super(message);
+    public DuplicateResourceException(String msg) {
+        super(msg);
     }
 }
