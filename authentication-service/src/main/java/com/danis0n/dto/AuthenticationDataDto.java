6f1a2885c4ac0b897087bc3d04e4bc0ac6f9ecfd
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-03 10:54:16 +0300

    moved to dto package

--- authentication-service/src/main/java/com/danis0n/dto/AuthenticationDataDto.java
+++ authentication-service/src/main/java/com/danis0n/dto/AuthenticationDataDto.java
@@ -1,11 +1,9 @@
 package com.danis0n.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
-public class AuthenticationData {
+public class AuthenticationDataDto {
     private String username;
     private String hashedPassword;
 }
--- user-service/src/main/java/com/danis0n/dto/AuthenticationDataDto.java
+++ user-service/src/main/java/com/danis0n/dto/AuthenticationDataDto.java
@@ -1,7 +1,9 @@
-package com.danis0n.entity;
+package com.danis0n.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class AuthenticationDataDto {
     private String username;
