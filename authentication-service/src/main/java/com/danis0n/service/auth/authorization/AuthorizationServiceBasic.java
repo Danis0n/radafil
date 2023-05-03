c317a1a4aad8b1523d7d4da0baea0bf866563fbc
Daniel Sopriko <sopriko.daniil@gmail.com>
2023-05-03 10:54:31 +0300

    fixed import

--- authentication-service/src/main/java/com/danis0n/service/auth/authorization/AuthorizationServiceBasic.java
+++ authentication-service/src/main/java/com/danis0n/service/auth/authorization/AuthorizationServiceBasic.java
@@ -1,7 +1,7 @@
 package com.danis0n.service.auth.authorization;
 
 import com.danis0n.client.UserClient;
-import com.danis0n.entity.AuthenticationDataDto;
+import com.danis0n.dto.AuthenticationDataDto;
 import com.danis0n.enums.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
